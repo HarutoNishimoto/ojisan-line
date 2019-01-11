c5419a8e57be35c903de9974416cc18b88585cc6
HarutoNishimoto <nh_mstrsib@yahoo.co.jp>
2019-01-11 19:33:23 +0900

    多国籍語に対応

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+global NAME
 NAME = "nishimoto"
 
 
